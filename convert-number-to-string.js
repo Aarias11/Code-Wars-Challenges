768e29f79f1e1e09ac539341f69cb7784cd40b9b
Aarias11 <108088290+Aarias11@users.noreply.github.com>
2023-03-03 10:42:24 -0500

    Add files via upload

---
+++ convert-number-to-string.js
@@ -0,0 +1,9 @@
+// **We need a function that can transform a number (integer) into a string. **
+
+
+function numberToString(num) {
+    let str = num.toString()
+    return str
+  }
+  
+  console.log(22)
