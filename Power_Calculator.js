cd115b9247f9b5f62e65ab5cee88fbb3070b9188
Aarias11 <108088290+Aarias11@users.noreply.github.com>
2023-03-24 09:48:25 -0400

    Add files via upload

---
+++ Power_Calculator.js
@@ -0,0 +1,19 @@
+// Create a function that takes voltage and current and returns the calculated power.
+
+// Examples
+// circuitPower(230, 10) ➞ 2300
+
+// circuitPower(110, 3) ➞ 330
+
+// circuitPower(480, 20) ➞ 9600
+
+
+
+
+//Find the formula for power
+//Formula: voltage * current = power
+
+function circuitPower(voltage, current) {
+	const power = voltage * current;
+	return power
+}
